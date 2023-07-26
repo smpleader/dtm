0b11661816025227fb870c9b67a7fdc797c6ab2b
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-07-26 08:58:43 +0700

    update modal new note

--- src/milestone/views/layouts/backend/relate_note/list/index.php
+++ src/milestone/views/layouts/backend/relate_note/list/index.php
@@ -74,5 +74,6 @@
     <input type="hidden" value="<?php echo $this->token ?>" name="token">
     <input type="hidden" value="DELETE" name="_method">
 </form>
-<?php echo $this->render('backend.relate_note.form', []); ?>
+<?php echo $this->render('backend.relate_note.form'); ?>
+<?php echo $this->renderWidget('note2::backend.note_modal'); ?>
 <?php echo $this->render('backend.relate_note.list.javascript', ['link_update_relate_note' => $this->link_update_relate_note]); ?>
--- src/milestone/views/layouts/backend/request/detail_request/index.php
+++ src/milestone/views/layouts/backend/request/detail_request/index.php
@@ -83,23 +83,4 @@ echo $this->render('backend.version_latest.list', []);
         </div>
     </div>
 </div>
-<div class="modal fade" id="noteNewModal" aria-labelledby="noteNewModalTitle" role="dialog">
-	<div class="modal-dialog modal-dialog-centered">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title" id="noteNewModalTitle">New Note</h4>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<div class="modal-body">
-				<div class="d-flex justify-content-around">
-				<?php foreach($this->note_types as $type) : ?>
-					<h4>
-						<a  target="_blank" class="mx-3" href="<?php echo $type['link']?>"><?php echo $type['title']?></a>
-					</h4>
-				<?php endforeach; ?>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 <?php echo $this->render('backend.request.detail_request.javascript'); ?>
