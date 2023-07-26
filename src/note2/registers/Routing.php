6a57b05c59380e4829d69d296a3af0a9e9662acb
joomaio.tinhcv <cvtinh2000@gmail.com>
2023-07-26 10:05:40 +0700

    create ajax get types

--- src/note2/registers/Routing.php
+++ src/note2/registers/Routing.php
@@ -9,6 +9,11 @@ class Routing
     public static function registerEndpoints()
     {
         return [
+            'note2/types' => [
+                'fnc' => [
+                    'get' => 'note2.ajax.types',
+                ]
+            ],
             'note2' => [
                 'fnc' => [
                     'get' => 'note2.note.list',
