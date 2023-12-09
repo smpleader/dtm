dec158c587fad9d03cb7e0c939de94f2b36b1247
tinh chu <cvtinh2000@gmail.com>
2023-12-09 15:45:26 +0700

    add icon preview in list

--- src/filter/views/layouts/filter/list/row.php
+++ src/filter/views/layouts/filter/list/row.php
@@ -3,7 +3,9 @@
         <input class="checkbox-item" type="checkbox" name="ids[]" value="<?php echo $this->item['id']; ?>">
     </td>
     <td>
-        <a href="<?php echo $this->link_view . '/' . strtolower($this->item['filter_link']); ?>"><?php echo  $this->item['name']  ?></a>
+        <a href="<?php echo $this->link_view . '/' . strtolower($this->item['filter_link']); ?>">
+            <i class="fa-solid fa-eye me-2"></i><?php echo  $this->item['name']  ?>
+        </a>
     </td>
     <td><?php echo $this->item['created_at'] ?></td>
     <td>
